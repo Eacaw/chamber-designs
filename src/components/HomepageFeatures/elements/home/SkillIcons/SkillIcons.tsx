5396eb2676d8b13bfe470f95a437f46a343da07b
David Pinchen <davidpinchen@gmail.com>
2024-01-03 07:56:08 +0000

    Move icons into separate SVG JSON file

---
+++ src/components/HomepageFeatures/elements/home/SkillIcons/SkillIcons.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import svgData from "./icons.json";
+
+type iconData = {
+  name: string;
+  path: string;
+};
+
+export default function SkillIcons(): JSX.Element {
+  const icons: [iconData] = svgData.icons;
+  return (
+    <div>
+      {icons.map((icon) => (
+        <svg
+          stroke="white"
+          fill="white"
+          stroke-width="0"
+          role="img"
+          viewBox="0 0 24 24"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>{icon.name}</title>
+          <path d={icon.path}></path>
+        </svg>
+      ))}
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 import styles from "./index.module.css";
-import SkillIcons from "../components/HomepageFeatures/elements/home/SkillIcons";
+import SkillIcons from "../components/HomepageFeatures/elements/home/SkillIcons/SkillIcons";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
