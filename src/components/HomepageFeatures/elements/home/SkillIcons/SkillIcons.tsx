a503624b8722136304b3fe8704e8ab071bfb0aa6
David Pinchen <davidpinchen@gmail.com>
2024-01-06 17:28:44 +0000

    Wrap icons

--- src/components/HomepageFeatures/elements/home/SkillIcons/SkillIcons.tsx
+++ src/components/HomepageFeatures/elements/home/SkillIcons/SkillIcons.tsx
@@ -9,22 +9,24 @@ type iconData = {
 export default function SkillIcons(): JSX.Element {
   const icons: iconData[] = svgData.icons;
   return (
-    <div>
-      {icons.map((icon) => (
-        <svg
-          stroke="white"
-          fill="white"
-          stroke-width="0"
-          role="img"
-          viewBox="0 0 24 24"
-          height="1em"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>{icon.name}</title>
-          <path d={icon.path}></path>
-        </svg>
-      ))}
-    </div>
+    <>
+      <div>
+        {icons.map((icon) => (
+          <svg
+            stroke="white"
+            fill="white"
+            stroke-width="0"
+            role="img"
+            viewBox="0 0 24 24"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>{icon.name}</title>
+            <path d={icon.path}></path>
+          </svg>
+        ))}
+      </div>
+    </>
   );
 }
