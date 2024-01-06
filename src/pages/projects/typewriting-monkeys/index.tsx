c9f85388641ea5d7e34c8899b104052eb15388d5
David Pinchen <davidpinchen@gmail.com>
2024-01-06 17:30:27 +0000

    Add rough frontent to twm

--- src/pages/projects/typewriting-monkeys/index.tsx
+++ src/pages/projects/typewriting-monkeys/index.tsx
@@ -2,12 +2,30 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import React from "react";
 
+import { runSimulation } from "./src/TypewritingMonkeys";
+
 import styles from "./tw-monkeys.module.css";
 import TopFiveDisplay from "../../../components/TypewritingMonkeys/TopFiveDisplay";
+import { Button, Card, Col, Row } from "antd";
+import SelectionTab from "./components/SelectionTab";
 
 export default function projects(): JSX.Element {
+  const defaultGenes = ["---", "---", "---", "---", "---"];
   const { siteConfig } = useDocusaurusContext();
-  const tempData = ["test1", "test2", "test3", "test4", "test5"];
+  const [topFive, setTopFive] = React.useState(defaultGenes);
+
+  const [generation, setGeneration] = React.useState(0);
+  const [disableButton, setDisableButton] = React.useState(false);
+
+  const simulationFinished = () => {
+    setDisableButton(false);
+  };
+
+  const startSimulation = () => {
+    runSimulation(setTopFive, setGeneration, simulationFinished);
+    setDisableButton(true);
+  };
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
@@ -15,7 +33,51 @@ export default function projects(): JSX.Element {
     >
       <div className={styles.content}>
         <main>
-          <TopFiveDisplay data={tempData} />
+          <Row gutter={16}>
+            <Col span={12}>
+              <Card
+                title="Top Five Gene Arrays"
+                style={{
+                  marginBottom: "1em",
+                }}
+              >
+                <TopFiveDisplay data={topFive} />
+                <h3
+                  style={{
+                    marginTop: "1em",
+                  }}
+                >
+                  Generations: {generation}
+                </h3>
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card
+                title="Options"
+                bordered={false}
+                style={{
+                  marginBottom: "1em",
+                }}
+              >
+                <SelectionTab />
+              </Card>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={24}>
+              <Card title="Controls" bordered={false}>
+                <div
+                  style={{
+                    height: "8em",
+                  }}
+                >
+                  <Button onClick={startSimulation} disabled={disableButton}>
+                    Click To run sim
+                  </Button>
+                </div>
+              </Card>
+            </Col>
+          </Row>
         </main>
       </div>
     </Layout>
