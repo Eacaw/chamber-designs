ef20e595fb9257177084b71bc7c40c154b95a330
David Pinchen <davidpinchen@gmail.com>
2024-01-07 16:03:03 +0000

    Add TextFormItem UI component

---
+++ src/pages/projects/typewriting-monkeys/components/UI/TextFormItem.tsx
@@ -0,0 +1,50 @@
+import { Col, Form, Input, InputNumber, Row, Slider } from "antd";
+import React from "react";
+
+interface props {
+  parentOnChange: (newValue: string) => void;
+  placeholder: string;
+  name: string;
+  label: string;
+  tooltip: string;
+}
+
+/**
+ * @param parentOnChange Method that will handle the onChange event in the parent Form
+ * @param min Minimum value that can be entered
+ * @param max Maximum value that can be entered
+ * @param placeholder Default value to show as a placeholder
+ * @param name Name of the form input
+ * @param label Label text of the input
+ * @param tooltip Tooltip text of the input
+ *
+ * @returns An Ant Design Form.Item both a slider and number input that are synced
+ */
+function SlidingNumberFormItem(props: props): JSX.Element {
+  const {
+    parentOnChange = null,
+    placeholder = 42,
+    name = "slidingNumberInput",
+    label = "slidingNumberInput",
+    tooltip = null,
+  } = props;
+
+  const onChange = (newValue: string) => {
+    parentOnChange(newValue);
+  };
+
+  return (
+    <Form.Item name={name} label={label} tooltip={tooltip}>
+      <Input
+        count={{
+          show: true,
+          max: 100,
+        }}
+        defaultValue={placeholder}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </Form.Item>
+  );
+}
+
+export default SlidingNumberFormItem;
