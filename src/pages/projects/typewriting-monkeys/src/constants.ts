ffbeb0d0738cb25ae7f77a15bb75c9289a5ca8bd
David Pinchen <davidpinchen@gmail.com>
2024-01-08 17:29:31 +0000

    Consolidate constants files

--- src/pages/projects/typewriting-monkeys/src/constants.ts
+++ src/pages/projects/typewriting-monkeys/src/constants.ts
@@ -1,5 +1,15 @@
 import { DNA } from "./DNA";
-import { DEFAULT_PHRASE } from "../components/constants";
+import { GASettings } from "../src/types";
+
+export const DEFAULT_PHRASE =
+  "This is a test sentence that is exactly one hundred characters long, including spaces & punctuation.";
+
+export const DEFAULT_GA_SETTINGS: GASettings = {
+  targetPhrase: DEFAULT_PHRASE,
+  populationSize: 2500,
+  mutationRate: 2,
+  elitismValue: 25,
+};
 
 export const DEFAULT_GENES: DNA[] = [
   new DNA(DEFAULT_PHRASE.split(""), 0),
